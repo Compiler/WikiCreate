54a5b8e4fe80436572c237ebae08bfb522ee4446
luke roche <lukerochedev@gmail.com>
2017-10-25 05:24:09 -0400

    see also section parsed

--- myscraper.py
+++ myscraper.py
@@ -26,9 +26,21 @@ def get_links(title):
                 final_link = valid_link.get('href')[6:]
                 if final_link not in global_link_hash: 
                     global_link_hash[final_link] = 1
-                    link_hash[final_link] = 1
+                    link_hash[final_link] = link_to_normal(final_link)
 
-    ##todo: add to link
+
+
+    ###########3
+    mything = 'div-col columns column-count column-count-2'
+    see_also_section = soupobj.find("div", {'class' : mything})
+    if see_also_section != None:
+        #see_also_section = see_also_section.findAll()
+        for element in see_also_section.contents:
+            if element.name == 'ul':
+                see_also_list = element.findAll('a', attrs={'href' : re.compile('^/wiki/')})
+                for links in see_also_list:
+                    print links.get('href')[6:]
+            ##todo: add to link
     
 
 
@@ -39,17 +51,17 @@ def get_links(title):
 
 def link_to_normal(link):
     link_key = link
-    link_key = final_link
     link_key = re.sub('_', ' ', link_key)
     link_key = urllib.unquote(link_key).decode('utf8')
     return link_key
 
 
 if __name__ == '__main__':
-    starting_link = 'Rabin-Karp algorithm'
+    #starting_link = 'Rabin-Karp algorithm'
+    starting_link = 'computer science'
     x=re.compile("^:wikt|outline|portal|list|sexual|latin|history|glossary|index|book|wikipedia|wikibooks|image|file|help|template|category|special:|english|language|\(disambiguation\)$")
     
-    first_level_hash = get('Rabin-Karp algorithm')
+    first_level_hash = get_links(starting_link)
     for v, k in first_level_hash.iteritems():
         print v, ' -> ', k
 
